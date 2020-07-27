432582f11e6f8de20042314e323c12b160b2dbd5
znseday <55926888+znseday@users.noreply.github.com>
2020-07-27 19:09:52 +0300

    Add files via upload

--- findskeleton.cpp
+++ findskeleton.cpp
@@ -49,7 +49,7 @@ bool FindSkeleton::LoadFromFile(const std::string &fn)
 
     MY_DEBUG_ONLY(
         cout << "Src Graph:" << endl;
-        for (const auto we : Graph)
+        for (const auto &we : Graph)
             cout << we << endl;
         cout << endl;
     )
@@ -81,19 +81,20 @@ vector<Edge> FindSkeleton::FindKruskal()
 
     MY_DEBUG_ONLY(
         cout << "Sorted:" << endl;
-        for (const auto we : Graph)
+        for (const auto &we : Graph)
             cout << we << endl;
         cout << endl;
     )
 
     int x, y;
-    for (size_t i = 0; i < Graph.size(); ++i)
+
+    for (const auto &e : Graph)
     {
-        x = Find(Graph[i].v1);
-        y = Find(Graph[i].v2);
+        x = Find(e.v1);
+        y = Find(e.v2);
         if ( x != y)
         {
-            res.emplace_back(Graph[i].v1, Graph[i].v2);
+            res.emplace_back(e.v1, e.v2);
             Union(x, y);
         }
     }
